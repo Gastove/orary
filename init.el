593e47ded53b23b9ff28fc7890e3f19bd9464a21
Ross Donaldson <gastove@gmail.com>
2016-04-26 10:02:33 -0700

    Add mu4e, exec-path-from-shell, work configs

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
 
 ;; Packages it just doesn't make sense to manage with use-package
 (defvar orary/packages '(dash
+			 exec-path-from-shell
 			 helm-projectile
 			 org-plus-contrib
 			 org-bullets
@@ -34,6 +35,10 @@
   (mapc #'orary/install-package orary/packages))
 
 (orary/install-packages)
+
+;;; OSX
+(require 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
 
 ;; OSX Modifier keys
 (setq mac-command-modifier 'meta)
@@ -281,6 +286,157 @@
   ;; Make sure I notice when I'm in
   (add-to-list 'rm-text-properties '(" Sp/s" 'face 'font-lock-warning-face)))
 
+;;; mu4e
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+
+(require 'mu4e)
+(require 'mu4e-contrib)
+(require 'org-mu4e)
+
+
+(setq mu4e-maildir "~/.Mail"
+      mu4e-drafts-folder "/gastove@gmail.com/[Gmail].Drafts"
+      mu4e-sent-folder   "/gastove@gmail.com/[Gmail].Sent Mail"
+
+      ;; don't save message to Sent Messages, Gmail/IMAP/Offlineimap takes care of this
+      mu4e-sent-messages-behavior 'delete
+
+      ;; Let offlineimap's autorefresh handle getting new mail, but automatically re-index:
+      mu4e-get-mail-command "offlineimap"
+
+      ;; Make mu4e the default user agent
+      mail-user-agent 'mu4e-user-agent
+
+      ;; fetch mail every 5 mins
+      mu4e-update-interval 300
+
+      ;; Name, main email address
+      user-mail-address "gastove@gmail.com"
+      user-full-name  "Ross Donaldson"
+
+      ;; Signature
+      mu4e-compose-signature (concat "Cheers,\n"
+				     "Ross\n")
+
+      ;; ISO date format for headers
+      mu4e-headers-date-format "%Y-%m-%d"
+
+      ;; Convert HTML emails to nicely readable text
+      mu4e-html2text-command 'mu4e-shr2text
+
+      ;; Technically not mu4e, but shr is hard to read:
+      shr-color-visible-luminance-min 80
+
+      ;; If the same email is in two different folders, don't show it twice
+      ;; in search results
+      mu4e-headers-skip-duplicates t
+
+      ;; Show images inline
+      mu4e-view-show-images t
+
+      ;; Silence the damn minibuffer updates
+      mu4e-hide-index-messages t
+
+      ;; Filter autocomplete addresses more intelligently
+      mu4e-compose-complete-only-after "2013-01-01"
+
+      ;; Capture better
+      org-mu4e-link-query-in-headers-mode nil)
+;; use imagemagick, if available
+(when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
+
+
+;; add option to view html message in a browser
+;; `aV` in view to activate
+(add-to-list 'mu4e-view-actions
+	     '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+
+;; configuration for sending mail
+(setq message-send-mail-function
+      ;; Send via postfix
+      'sendmail-send-it
+
+      ;; By default, the gnus composer (which mu4e uses also) doesn't correctly
+      ;; write FROM headers. Let's fix that. (Required to correctly dispatch to
+      ;; multiple sending accounts with postfix.)
+      mail-specify-envelope-from t
+      mail-envelope-from 'header)
+
+;; Message Composition settings
+(add-hook 'mu4e-compose-mode-hook
+	  (lambda ()
+	    (auto-fill-mode -1)
+	    (whitespace-mode -1)))
+
+;; The mu4e interface
+;; Single-character shortcuts
+(setq mu4e-maildir-shortcuts
+      '(("/gastove@gmail.com/INBOX"               . ?i)
+	("/gastove@gmail.com/[Gmail].Important"   . ?I)
+	("/gastove@gmail.com/[Gmail].Sent Mail"   . ?s)))
+
+
+;; Tweak bookmarked queries
+(add-to-list 'mu4e-bookmarks `(,(string-join
+				 '("flag:unread"
+				   "AND date:today..now"
+				   "NOT maildir:/ross@urbanairship.com/Githubs"
+				   "NOT maildir:'/ross@urbanairship.com/Sales Deals'"
+				   "AND m:/ross@urbanairship.com/INBOX")
+				 " ")
+			       "Today's work unreads" ?i))
+(add-to-list 'mu4e-bookmarks `(,(string-join
+				 '("flag:unread"
+				   "AND m:/gastove@gmail.com/INBOX"
+				   "AND date:today..now")
+				 " ") "Today's Personal Unreads" ?h))
+(add-to-list 'mu4e-bookmarks `(,(string-join
+				 '("flag:unread"
+				   "AND m:/gastove@gmail.com/INBOX"
+				   "AND date:today..now"
+				   "OR flag:unread"
+				   "AND m:/ross@urbanairship.com/INBOX"
+				   "AND date:today..now")
+				 " ") "Today's Unreads" ?u))
+(add-to-list 'mu4e-bookmarks `(,(string-join
+				 '("m:/gastove@gmail.com/INBOX"
+				   "AND date:10d..now"
+				   "or m:/ross@urbanairship.com/INBOX"
+				   "AND date:10d..now")
+				 " ") "Working Mail" ?w))
+
+(defvar my-mu4e-account-alist
+  '(("gastove@gmail.com"
+     (mu4e-drafts-folder "/gastove@gmail.com/[Gmail].Drafts")
+     (mu4e-sent-folder   "/gastove@gmail.com/[Gmail].Sent Mail")
+     (user-mail-address "gastove@gmail.com"))
+    ("ross@urbanairship.com"
+     (mu4e-drafts-folder "/ross@urbanairship.com/[Gmail].Drafts")
+     (mu4e-sent-folder   "/ross@urbanairship.com/[Gmail].Sent Mail")
+     (user-mail-address "ross@urbanairship.com"))))
+
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+	  (if mu4e-compose-parent-message
+	      (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+		(string-match "/\\(.*?\\)/" maildir)
+		(match-string 1 maildir))
+	    (completing-read (format "Compose with account: (%s) "
+				     (mapconcat #'(lambda (var) (car var))
+						my-mu4e-account-alist "/"))
+			     (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+			     nil t nil nil (caar my-mu4e-account-alist))))
+	 (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+	(mapc #'(lambda (var)
+		  (set (car var) (cadr var)))
+	      account-vars)
+      (error "No email account found"))))
+
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
 ;; Languages
 (defun orary/elisp-defaults ()
   (eldoc-mode +1)
@@ -378,6 +534,11 @@ If no region is selected and current line is not blank
 (global-set-key (kbd "C-x j") 'orary/insert-iso-date)
 (global-set-key (kbd "M-;") `comment-dwim-line)
 
+
+;; Local work configs
+(let ((work-configs (expand-file-name ".work.el" (getenv "HOME"))))
+  (when (file-exists-p work-configs)
+    (load-file work-configs)))
 
 ;; Last but not least, click on the server
 (server-start)
