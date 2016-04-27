ad76e637f6a5cd3a0e56be3c6be0022478b33be3
Ross Donaldson <gastove@gmail.com>
2016-04-26 23:15:59 -0700

    I feel like I've done this before

--- init.el
+++ init.el
@@ -80,6 +80,7 @@
 (setq-default save-place t)
 (setq save-place-file "~/.emacs.d/savefile/saved-places")
 
+
 ;; Remember command and file history
 (require 'savehist)
 (setq savehist-file "~/.emacs.d/savefile/savehist")
@@ -662,3 +663,17 @@ If no region is selected and current line is not blank
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Code/astromech/notes.org" "~/Dropbox/org-docs/cotidienne.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
