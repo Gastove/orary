f86873eb98c39218a502188e67a57ea84643358a
Ross Donaldson <gastove@gmail.com>
2016-04-28 13:57:23 -0700

    Expand emote repetoire

--- modules/orary-emote.el
+++ modules/orary-emote.el
@@ -9,7 +9,12 @@
 (defvar orary/emotes
   (helm-build-sync-source "Orary Emoji"
     :candidates '(("Flip table: (╯°□°）╯︵ ┻━┻ " . "(╯°□°）╯︵ ┻━┻ ")
-		  ("Angry table flip: (ノ□益□)ノ彡┻━┻ " . "(ノ□益□)ノ彡┻━┻ "))))
+		  ("Angry table flip: (ノ□益□)ノ彡┻━┻ " . "(ノ□益□)ノ彡┻━┻ ")
+		  ("Flip it back: ┬──┬ ﻿ノ( ゜-゜ノ)" . "┬──┬ ﻿ノ( ゜-゜ノ)")
+		  ("Shrug: ¯\_(ツ)_/¯" . "¯\_(ツ)_/¯")
+		  ("Music: ヽ(⌐■_■)ノ♪♬" . "ヽ(⌐■_■)ノ♪♬")
+		  ("Stuff to do: ᕕ(╯°□°)ᕗ" . "ᕕ(╯°□°)ᕗ")
+		  ("Stroll ᕕ( ᐛ )ᕗ" . "ᕕ( ᐛ )ᕗ"))))
 
 (defun orary/insert-emote ()
   (interactive)
