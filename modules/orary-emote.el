0b89062d9ccaebe8a9d05d29a1f7a9425e0dd944
Ross Donaldson <gastove@gmail.com>
2016-04-28 13:58:41 -0700

    Slightly cleaner syntax; easier to extend

--- modules/orary-emote.el
+++ modules/orary-emote.el
@@ -7,18 +7,18 @@
 (require 'helm)
 
 (defvar orary/emotes
-  (helm-build-sync-source "Orary Emoji"
-    :candidates '(("Flip table: (╯°□°）╯︵ ┻━┻ " . "(╯°□°）╯︵ ┻━┻ ")
+  '(("Flip table: (╯°□°）╯︵ ┻━┻ " . "(╯°□°）╯︵ ┻━┻ ")
 		  ("Angry table flip: (ノ□益□)ノ彡┻━┻ " . "(ノ□益□)ノ彡┻━┻ ")
 		  ("Flip it back: ┬──┬ ﻿ノ( ゜-゜ノ)" . "┬──┬ ﻿ノ( ゜-゜ノ)")
 		  ("Shrug: ¯\_(ツ)_/¯" . "¯\_(ツ)_/¯")
 		  ("Music: ヽ(⌐■_■)ノ♪♬" . "ヽ(⌐■_■)ノ♪♬")
 		  ("Stuff to do: ᕕ(╯°□°)ᕗ" . "ᕕ(╯°□°)ᕗ")
-		  ("Stroll ᕕ( ᐛ )ᕗ" . "ᕕ( ᐛ )ᕗ"))))
+		  ("Stroll ᕕ( ᐛ )ᕗ" . "ᕕ( ᐛ )ᕗ")))
 
 (defun orary/insert-emote ()
   (interactive)
-  (insert (helm :sources 'orary/emotes
+  (insert (helm :sources (helm-build-sync-source "Orary Emoji"
+			   :candidates 'orary/emotes)
 		:buffer "*Orary Emotes*")))
 
 (provide 'orary-emote)
