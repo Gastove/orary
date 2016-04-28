9ecee95f8168058634e88c9baf8b9e5041a91c75
Ross Donaldson <gastove@gmail.com>
2016-04-28 14:00:10 -0700

    squash! Lisp and programming defaults

--- modules/orary-lisp.el
+++ modules/orary-lisp.el
@@ -6,14 +6,11 @@
 
 ;;; Languages
 ;; Lisp
-(defun orary/elisp-defaults ()
+(defun orary/lisp-defaults ()
   (eldoc-mode +1)
   (rainbow-mode +1)
   (rainbow-delimiters-mode-enable)
-  (smartparens-strict-mode +1)
-  (define-key lisp-mode-map (kbd "C-c C-b") 'eval-buffer))
-
-(add-hook 'emacs-lisp-mode-hook #'orary/elisp-defaults)
+  (smartparens-strict-mode +1))
 
 (provide 'orary-lisp)
 ;;; orary-lisp.el ends here
