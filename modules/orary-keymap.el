013f8146402131e1f9c0a78b6b387daa9b801694
Ross Donaldson <gastove@gmail.com>
2016-04-28 22:56:48 -0700

    wire up emotes

--- modules/orary-keymap.el
+++ modules/orary-keymap.el
@@ -1,18 +1,18 @@
 ;;; orary-keymap.el ---
 ;;
 ;;; Commentary:
-;;
+;; Global keybindings in orary.
 ;;; Code:
 
 (global-set-key [remap move-beginning-of-line]
 		'orary/prelude-move-beginning-of-line)
 (global-set-key (kbd "s-j") 'orary/join-below)
-(global-set-key (kbd "C-c n") 'orary/clean-up-buffer)
-(global-set-key (kbd "C-x |") 'orary/toggle-window-split)
-(global-set-key (kbd "C-x j") 'orary/insert-iso-date)
 (global-set-key (kbd "M-;") 'orary/comment-dwim-line)
-(define-key python-mode-map (kbd "C-c q r") 'orary/replace-double-quote-with-single)
+(global-set-key (kbd "C-c n") 'orary/clean-up-buffer)
 
+(global-set-key (kbd "C-c q t") 'orary/toggle-window-split)
+(global-set-key (kbd "C-c q d") 'orary/insert-iso-date)
+(global-set-key (kbd "C-c q e") 'orary/insert-emote)
 
 (provide 'orary-keymap)
 ;;; orary-keymap.el ends here
