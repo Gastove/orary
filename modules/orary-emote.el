97212ad3c6c758d46bb697a1e51089ce22c66f56
Ross Donaldson <gastove@gmail.com>
2016-04-29 20:09:41 -0700

    Finally get this working in an Awesome Way
    ヽ(⌐■_■)ノ♪♬
    (●♡∀♡)

--- modules/orary-emote.el
+++ modules/orary-emote.el
@@ -7,9 +7,9 @@
 (require 'helm)
 
 (defvar orary/emotes
-  '(("Flip table: (╯°□°）╯︵ ┻━┻ " . "(╯°□°）╯︵ ┻━┻ ")
-    ("Angry table flip: (ノ□益□)ノ彡┻━┻ " . "(ノ□益□)ノ彡┻━┻ ")
-    ("Flip it back: ┬──┬ ﻿ノ( ゜-゜ノ)" . "┬──┬ ﻿ノ( ゜-゜ノ)")
+  '(("Flip table: (╯°□°）╯︵ ┻━┻ " . ("( °_°） ┬──┬ " "(╯°□°）╯︵ ┻━┻ "))
+    ("Angry table flip: (ノ□益□)ノ彡┻━┻ " . ("( °_°） ┬──┬ " "(-°□°）-┬──┬ " "(ノ□益□)ノ彡┻━┻ "))
+    ("Flip it back: ┬──┬ ﻿ノ( ゜-゜ノ)" . ("┻━━┻    ( -.-  )" "┻━━┻  ノ( ゜-゜ノ)" "┬──┬    (  ^_^  )" "┬──┬ ﻿ノ( ゜-゜ノ)"))
     ("Flip two ┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻" . "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻")
     ("Shrug: ¯\_(ツ)_/¯" . "¯\_(ツ)_/¯")
     ("Music: ヽ(⌐■_■)ノ♪♬" . "ヽ(⌐■_■)ノ♪♬")
@@ -19,12 +19,31 @@
     ("wut ⊙△⊙" . "⊙△⊙")
     ("Bring it (ง •̀_•́)ง" . "(ง •̀_•́)ง")))
 
+(defun orary/emote-insert-emote (emotes)
+  "Entertains you with messaged emotes, then copies the final
+emote from EMOTES to the kill ring and inserts it."
+  (if (listp emotes)
+      (progn
+        (dolist (emote emotes)
+         (message emote)
+         (sleep-for 0 500))
+       (let ((final (car (last emotes))))
+         (kill-new final)
+         (insert final)))
+    (kill-new emotes)
+    (insert emotes)))
+
+(defvar orary/emote-actions
+  (helm-make-actions "Insert + copy to kill ring" 'orary/emote-insert-emote
+                     "Copy to kill ring" 'kill-new))
+
 (defun orary/insert-emote ()
   "Allow for the selection and insertion of handy emotes."
   (interactive)
-  (insert (helm :sources (helm-build-sync-source "Orary Emoji"
-			  :candidates 'orary/emotes)
-		:buffer "*Orary Emotes*")))
+  (helm :sources (helm-build-sync-source "Orary Emoji"
+                   :candidates 'orary/emotes
+                   :action 'orary/emote-actions)
+        :buffer "*Orary Emotes*"))
 
 (provide 'orary-emote)
 ;;; orary-emote.el ends here
