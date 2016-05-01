1545fddb1a496cf2bdc256fc1cff0c36ff683a38
Ross Donaldson <gastove@gmail.com>
2016-04-30 22:59:56 -0700

    Start tracking orary-editor

---
+++ modules/orary-editor.el
@@ -0,0 +1,83 @@
+;;; orary-editor.el --- Configuring editor behaviors for src + txt
+;;
+;;; Commentary:
+;; Some behaviors should go all through `orary'. In general, thins bring in
+;; tools that could apply to many languages (e.g. FlyCheck), but per-language
+;; configuration is in the corresponding orary module.
+;;; Code:
+
+(require 'dash)
+(require 'f)
+
+(use-package easy-kill
+  :config
+  (global-set-key [remap kill-ring-save] 'easy-kill))
+
+(use-package flycheck
+  :config (global-flycheck-mode))
+
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :config
+  (yas-global-mode 1)
+  (setq yas-prompt-functions '(yas-completing-prompt)))
+
+(use-package wgrep
+  :demand t)
+
+(use-package wgrep-helm
+  :demand t)
+
+(use-package wgrep-ag
+  :demand t
+  :config
+  (autoload 'wgrep-ag-setup "wgrep-ag")
+  (add-hook 'ag-mode-hook 'wgrep-ag-setup)
+  (add-hook 'helm-ag-mode-hook 'wgrep-ag-setup))
+
+;;----------------------------------Flyspell------------------------------------
+;; Spelling Checkingz
+(require 'flyspell)
+(setq ispell-program-name "aspell"
+      ;; The default mode is too slow, but `ultra' is too imprecise.
+      ispell-extra-args '("--sug-mode=fast"))
+
+(defun orary/flyspell-text ()
+  "Enable flyspell for text modes."
+  (when (executable-find ispell-program-name)
+    (flyspell-mode +1)))
+
+(defun orary/flyspell-programming ()
+  "Enable flyspell for programming modes."
+  (when (executable-find ispell-program-name)
+    (flyspell-prog-mode)))
+
+(add-hook 'prog-mode-hook 'orary/flyspell-programming)
+(add-hook 'text-mode-hook 'orary/flyspell-text)
+
+(use-package rainbow-mode
+  :diminish rainbow-mode)
+
+;;; Undo
+(use-package undo-tree
+  :demand t
+  :config
+  (global-undo-tree-mode)
+  (-let [undo-tree-dir (f-expand "undo-tree" orary/save-root)]
+    (unless (f-exists? undo-tree-dir)
+      (f-mkdir undo-tree-dir))
+    (setq undo-tree-history-directory-alist `((".*" . ,undo-tree-dir))
+          undo-tree-auto-save-history t))
+  :bind ("C-c u" . undo-tree-visualize))
+
+;; Another fine trick from Bozhidar Batsov
+(defadvice exchange-point-and-mark (before deactivate-mark activate compile)
+  "When called with no active region, do not activate mark."
+  (interactive
+   (list (not (region-active-p)))))
+
+;; Always revert a buffer if the underlying file has changed on disk
+(global-auto-revert-mode t)
+
+(provide 'orary-editor)
+;;; orary-editor.el ends here
