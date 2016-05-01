4a8ee3f3ffa4499e315b10fdee7b3ea636467500
Ross Donaldson <gastove@gmail.com>
2016-04-30 23:15:21 -0700

    all ordering and whitespace

--- init.el
+++ init.el
@@ -4,8 +4,7 @@
 ;;  Yep.  We got code.
 ;;; Code:
 
-;;; Package Management
-
+;;-----------------------------Package Management-------------------------------
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
@@ -13,12 +12,6 @@
 (add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 
 (package-initialize)
-
-;; We're about to use this
-(defvar orary/orary-root-dir (file-name-directory
-                              (or (buffer-file-name) (file-chase-links load-file-name))))
-
-(add-to-list 'load-path (expand-file-name "modules/" orary/orary-root-dir))
 
 ;; Make sure the package index has been loaded at least once before we try to
 ;; do any installing. After, make a note that we don't have to do this again.
@@ -29,42 +22,51 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
+(require 'use-package)
+
+;; Package Loading and Configuration
+(setq use-package-always-ensure t)
+
 ;; New Core
 (use-package dash)                      ;; Modern FP, combiners
 (use-package s)                         ;; String manipulation
 (use-package f)                         ;; File manipulation
 
-;; Package Loading and Configuration
-(setq use-package-always-ensure t)
+;; We're about to use this
+(defvar orary/orary-root-dir (file-name-directory
+                              (or (buffer-file-name) (file-chase-links load-file-name))))
+
+(add-to-list 'load-path (f-expand "modules/" orary/orary-root-dir))
 
 ;; Load this first in case things need executables from the path.
 (require 'orary-osx)
 
-(require 'orary-company)
+
 (require 'orary-core)
 (require 'orary-ui)
+
 (require 'orary-editor)
-
 (require 'orary-programming)
-(require 'orary-lisp)
+(require 'orary-text)
 
-(require 'orary-helm)
-(require 'orary-magit)
-(require 'orary-org)
-
+(require 'orary-company)
 (require 'orary-emote)
+(require 'orary-functions)
+(require 'orary-helm)
 (require 'orary-jabber)
+(require 'orary-keymap)
+(require 'orary-magit)
+(require 'orary-mu4e)
+(require 'orary-org)
 (require 'orary-projectile)
 (require 'orary-smartparens)
-(require 'orary-mu4e)
+(require 'orary-work)
+
 (require 'orary-emacs-lisp)
+(require 'orary-nxml)
+(require 'orary-purescript)
 (require 'orary-python)
 (require 'orary-scala)
-(require 'orary-purescript)
-(require 'orary-nxml)
-(require 'orary-functions)
-(require 'orary-keymap)
-(require 'orary-work)
 
 ;; Last but not least, click on the server
 (server-start)
