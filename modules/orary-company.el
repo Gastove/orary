a2028b6b472b60957d095de60ac8a96eac32c5bb
Ross Donaldson <gastove@gmail.com>
2016-05-01 22:16:03 -0700

    A license! More completion frameworks

--- modules/orary-company.el
+++ modules/orary-company.el
@@ -1,7 +1,25 @@
 ;;; orary-company.el --- company-mode in Orary
 ;;
+;; Copyright (C) 2016 Ross Donaldson
+
+;; Author: Ross Donaldson <gastove@gmail.com>
+;; URL: https://github.com/Gastove/orary
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;;
+;; Integrate company-mode in to orary for ALL the autocompletes.
 ;;; Code:
 
 (use-package company
@@ -13,6 +31,9 @@
 	company-minimum-prefix-length 2
 	company-tooltip-limit 20)
   :diminish company)
+
+(use-package company-jedi)
+(use-package company-tern)
 
 (provide 'orary-company)
 ;;; orary-company.el ends here
