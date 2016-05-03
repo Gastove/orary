b8edf44f30f1dbe74149bf2cc2f3d9f0d6e1fbc2
Ross Donaldson <gastove@gmail.com>
2016-05-03 13:27:16 -0700

    White space, splitting, fuzzy matching

--- modules/orary-helm.el
+++ modules/orary-helm.el
@@ -18,13 +18,15 @@
   (require 'helm-config)
   (require 'helm-projectile)
   (helm-mode 1)
-  (setq helm-split-window-in-side-p       t
-    helm-split-window-default-side        'below
-    helm-split-window-in-side-p           t
-    helm-buffers-fuzzy-matching           t
-    helm-move-to-line-cycle-in-source     t
-    helm-ff-search-library-in-sexp        t
-    helm-ff-file-name-history-use-recentf t)
+  (setq helm-split-window-in-side-p            t
+        helm-split-window-default-side         'below
+        helm-move-to-line-cycle-in-source      t
+        helm-ff-search-library-in-sexp         t
+        helm-ff-file-name-history-use-recentf  t
+        ;; Fuzzy Matching
+        helm-buffers-fuzzy-matching            t
+        helm-M-x-fuzzy-match                   t
+        helm-recentf-fuzzy-match               t)
   (when (executable-find "curl")
     (setq helm-google-suggest-use-curl-p t))
   (setq projectile-completion-system 'helm)
@@ -32,20 +34,20 @@
   (helm-descbinds-mode)
   (global-set-key (kbd "C-c h") 'helm-command-prefix)
   (global-unset-key (kbd "C-x c"))
-  :bind (("M-x" . helm-M-x)
-     ("M-y" . helm-show-kill-ring)
-     ("C-x b" . helm-mini)
-     ("C-c f" . helm-recentf)
-     ("C-x C-f" . helm-find-files)
-     ("C-h f" . helm-apropos)
-     :map helm-map
-     ([tab] . helm-execute-persistent-action)
-     ("C-i" . helm-execute-persistent-action)
-     ("C-z" . helm-select-action)
-     :map helm-command-map
-     ("o" . helm-occur)
-     ("g" . helm-do-grep)
-     ("SPC" . helm-all-mark-rings)))
+  :bind (("M-x"      . helm-M-x)
+         ("M-y"      . helm-show-kill-ring)
+         ("C-x b"    . helm-mini)
+         ("C-c f"    . helm-recentf)
+         ("C-x C-f"  . helm-find-files)
+         ("C-h f"    . helm-apropos)
+         :map helm-map
+         ([tab]      . helm-execute-persistent-action)
+         ("C-i"      . helm-execute-persistent-action)
+         ("C-z"      . helm-select-action)
+         :map helm-command-map
+         ("o"        . helm-occur)
+         ("g"        . helm-do-grep)
+         ("SPC"      . helm-all-mark-rings)))
 
 (provide 'orary-helm)
 ;;; orary-helm.el ends here
