fc8339134397ffa12133f90666e8c4a389857add
Ross Donaldson <gastove@gmail.com>
2016-05-11 18:41:26 -0700

    GPL, tweak

--- modules/orary-emote.el
+++ modules/orary-emote.el
@@ -1,5 +1,23 @@
 ;;; orary-emote.el --- Emotes!
 ;;
+;; Copyright (C) 2016 Ross Donaldson
+
+;; Author: Ross Donaldson <gastove@gmail.com>
+;; URL: https://github.com/Gastove/orary
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;;; Code:
@@ -32,8 +50,9 @@ emote from EMOTES to the kill ring and inserts it."
        (let ((final (car (last emotes))))
          (kill-new final)
          (insert final)))
-    (kill-new emotes)
-    (insert emotes)))
+    (progn
+     (kill-new emotes)
+     (insert emotes))))
 
 (defvar orary/emote-actions
   (helm-make-actions "Insert + copy to kill ring" 'orary/emote-insert-emote
