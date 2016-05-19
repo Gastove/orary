33fa0b62622f5fa49e9039063c3061677db4a5de
Ross Donaldson <gastove@gmail.com>
2016-05-19 14:22:24 -0700

    staaaahp

--- modules/orary-lisp.el
+++ modules/orary-lisp.el
@@ -6,15 +6,20 @@
 
 ;;; Languages
 ;; Lisp
+
+(require 'diminish)
+
 (defun orary/lisp-defaults ()
   "Set default values for lisp-language major modes."
   (eldoc-mode +1)
   (subword-mode +1)
+  (diminish 'subword-mode)
   (rainbow-delimiters-mode +1)
   (smartparens-strict-mode +1))
 
 (defun orary/interactive-lisp-defaults ()
   (subword-mode +1)
+  (diminish 'subword-mode)
   (rainbow-delimiters-mode +1)
   (smartparens-strict-mode +1))
 
