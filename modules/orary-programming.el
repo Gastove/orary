853b68b3f7703b6dd2214dee4339499926e78a7d
Ross Donaldson <gastove@gmail.com>
2016-05-27 11:05:35 -0700

    more indent-sensitive modes

--- modules/orary-programming.el
+++ modules/orary-programming.el
@@ -29,7 +29,7 @@ Currently catches: FIX(ME)?, TODO, NOTE."
 (defvar orary/gg-tags-modes
   '(c-mode 'c++-mode java-mode emacs-lisp-mode))
 
-(defvar orary/indent-sensitive-modes '(conf-mode yaml-mode))
+(defvar orary/indent-sensitive-modes '(conf-mode yaml-mode scala-mode purescript-mode))
 
 (defvar orary/disable-auto-indent nil)
 (make-variable-buffer-local 'orary/disable-auto-indent)
