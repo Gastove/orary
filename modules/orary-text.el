de059e8471b49bfbb9a1dd7f261c36fc3832f4fa
Ross Donaldson <gastove@gmail.com>
2016-05-28 10:41:13 -0700

    Add markdown, rst to text

--- modules/orary-text.el
+++ modules/orary-text.el
@@ -6,5 +6,14 @@
 
 ;; Er.
 
+(use-package markdown-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.markdown\\'". gfm-mode))
+  (add-to-list 'auto-mode-alist '("\\.mdown\\'". gfm-mode))
+  (add-to-list 'auto-mode-alist '("\\.md\\'". gfm-mode)))
+
+;; TODO: This is a good idea. Get this cleaned up.
+;; (use-package rst)
+
 (provide 'orary-text)
 ;;; orary-text.el ends here
