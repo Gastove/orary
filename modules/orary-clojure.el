56f336aab8c6a3569baca5d1db94e1577420d3fe
Ross Donaldson <gastove@gmail.com>
2016-05-31 20:05:09 -0700

    Unhork clojure configs

--- modules/orary-clojure.el
+++ modules/orary-clojure.el
@@ -6,27 +6,31 @@
 
 (require 'orary-lisp)
 
+;; clojure-mode comes with clojurescript-mode baked in
 (use-package clojure-mode
-  :mode "\\.cljs?\\'"
   :config
   (add-hook 'clojure-mode-hook 'orary/lisp-defaults)
-  (add-hook 'clojure-mode-hook 'cider-mode))
+  ;; (add-hook 'clojure-mode-hook 'cider-mode)
+  )
+
+(add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojurescript-mode))
+(add-hook 'clojurescript-mode-hook 'orary/lisp-defaults)
+(add-hook 'clojurescript-mode-hook 'cider-mode)
+(setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
 
 (use-package cider
   :defer t
   :config
   (setq nrepl-log-messages t
         cider-repl-display-help-banner) nil
-  (add-hook 'cider-mode-hook 'eldoc-mode)
-  (add-hook 'cider-repl-mode-hook 'orary/interactive-lisp-defaults))
+        (add-hook 'cider-mode-hook 'eldoc-mode)
+        (add-hook 'cider-repl-mode-hook 'orary/interactive-lisp-defaults))
 
 (use-package clj-refactor
   :config
   (add-hook 'clojure-mode-hook
             (lambda ()
               (clj-refactor-mode 1)
-              (add-hook 'cider-connected-hook #'cljr-update-artifact-cache)
-              (add-hook 'cider-connected-hook #'cljr-warm-ast-cache)
               (cljr-add-keybindings-with-prefix "s-r"))))
 
 (provide 'orary-clojure)
