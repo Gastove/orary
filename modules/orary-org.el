234ef7da5950ee6f9e734e182424c03f61f3a145
Ross Donaldson <gastove@gmail.com>
2016-07-05 11:36:27 -0700

    Capture is being fucky; be more explicit about capture files

--- modules/orary-org.el
+++ modules/orary-org.el
@@ -1,8 +1,28 @@
 ;;; orary-org.el --- Configuration for org-mode in Orary
 ;;
+;; Copyright (C) 2016 Ross Donaldson
+
+;; Author: Ross Donaldson <gastove@gmail.com>
+;; URL: https://github.com/Gastove/orary
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;  Make Org Go!
 ;;; Code:
+
+(require 'f)
 
 ;; Org
 (defun add-pcomplete-to-capf ()
@@ -83,7 +103,7 @@
         ;; Fontify inside code blocks
         org-src-fontify-natively t
 
-        org-default-notes-file "~/Dropbox/org-docs/cotidienne.org"
+        org-default-notes-file (f-expand"~/Dropbox/org-docs/cotidienne.org")
 
         org-capture-templates
         `(("t" "Todo" entry (file+headline "" "General To-Dos")
