37d991eaad5b7da64130d93c5e009af4540ed783
Ross Donaldson <gastove@gmail.com>
2016-08-19 10:59:01 -0700

    Wire up Marked.app

--- modules/orary-text.el
+++ modules/orary-text.el
@@ -8,9 +8,17 @@
 
 (use-package markdown-mode
   :config
+  (defun orary/markdown-preview-file ()
+    "Run Marked on the current file and revert the buffer."
+    (interactive)
+    (shell-command
+     (format "open -a '/Applications/Marked 2.app' %s"
+             (shell-quote-argument (buffer-file-name)))))
   (add-to-list 'auto-mode-alist '("\\.markdown\\'". gfm-mode))
   (add-to-list 'auto-mode-alist '("\\.mdown\\'". gfm-mode))
-  (add-to-list 'auto-mode-alist '("\\.md\\'". gfm-mode)))
+  (add-to-list 'auto-mode-alist '("\\.md\\'". gfm-mode))
+  :bind (("C-c m" . orary/markdown-preview-file)))
+
 
 ;; TODO: This is a good idea. Get this cleaned up.
 ;; (use-package rst)
