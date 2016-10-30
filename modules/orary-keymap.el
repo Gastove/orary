c11af173163309e7901da9752159404c761ac9db
Ross Donaldson <gastove@gmail.com>
2016-10-30 12:22:13 -0700

    Wire up json-pprint func, add shortcut for toggling fullscreen

--- modules/orary-keymap.el
+++ modules/orary-keymap.el
@@ -5,7 +5,7 @@
 ;;; Code:
 
 (global-set-key [remap move-beginning-of-line]
-		'orary/prelude-move-beginning-of-line)
+        'orary/prelude-move-beginning-of-line)
 (global-set-key (kbd "s-j") 'orary/join-below)
 (global-set-key (kbd "M-;") 'orary/comment-dwim-line)
 (global-set-key (kbd "C-c n") 'orary/clean-and-indent-buffer)
@@ -13,6 +13,8 @@
 (global-set-key (kbd "C-c q t") 'orary/toggle-window-split)
 (global-set-key (kbd "C-c q d") 'orary/insert-iso-date)
 (global-set-key (kbd "C-c q e") 'orary/insert-emote)
+(global-set-key (kbd "C-c q s") 'toggle-frame-fullscreen)
+(global-set-key (kbd "C-c q j") 'orary/pprint-json-in-new-buffer)
 (global-set-key (kbd "C-;") 'flyspell-auto-correct-previous-word)
 
 (provide 'orary-keymap)
