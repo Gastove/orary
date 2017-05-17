03509fd0ead5bd899904c17affdfc7dfceb7d1ce
Ross Donaldson <gastove@gmail.com>
2017-05-16 20:04:02 -0700

    Indentation got wacky? Unwacky it.

--- modules/orary-magit.el
+++ modules/orary-magit.el
@@ -8,15 +8,15 @@
 (use-package magit
   :config
   (setq magit-last-seen-setup-instructions "1.4.0"
-	magit-branch-read-upstream-first t
-	magit-branch-arguments nil
-	magit-push-arguments '("--set-upstream")
-	magit-push-always-verify nil
-	magit-revert-buffers t)
+        magit-branch-read-upstream-first t
+        magit-branch-arguments nil
+        magit-push-arguments '("--set-upstream")
+        magit-push-always-verify nil
+        magit-revert-buffers t)
   :bind (("s-m m" . magit-status)
-	 ("C-x g" . magit-status)
-	 ("s-m l" . magit-log)
-	 ("s-m b" . magit-blame)))
+         ("C-x g" . magit-status)
+         ("s-m l" . magit-log)
+         ("s-m b" . magit-blame)))
 
 (use-package git-timemachine
   :bind ("s-m t" . git-timemachine))
