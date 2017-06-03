65d69ff4f10de55132fab84e29251251043b1ea4
Ross Donaldson <gastove@gmail.com>
2017-06-03 12:27:03 -0700

    Keep improving F# sp pairs, fsharp-ret-dwim func

--- modules/orary-fsharp.el
+++ modules/orary-fsharp.el
@@ -20,33 +20,49 @@
          (not (bolp))
          (looking-back "<\\|<@\\{1,2\\}" 2))))
 
-;; Hrm: \\|^type [A-Za-z'_]+ =
+(defun orary/sp-close-open-angle-p (delim action ctxt)
+  (save-excursion
+    (looking-back "[^|-]" 2)))
+
+(defun orary/sp-correct-angle-bracket (delim action ctxt)
+  (save-excursion
+    (when (looking-at-p ">@")
+      (delete-char 2)
+      (insert "@>"))))
+
 (defun fsharp-ret-dwim ()
   "Look for an operator in the currently line -- one of |, |>,
 ||>, |||>. Insert a newline after current line, indent, and
-insert the found character. Note: this function currently does
-not support types/classes, even though it would be consistent
-from a usability standpoint to do so."
+insert the found character, along with a proper yasnippet for
+types and match blocks. Note: this function currently does not
+support classes, even though it would be consistent from a
+usability standpoint to do so."
   (interactive)
   (let ((search-bol (line-beginning-position))
         (search-eol (line-end-position))
+        (type-yas-tpl "$1 of $0")
+        (match-yas-tpl "$1 -> $0")
         insertion-char
         insertion-tpl)
     (beginning-of-line)
-    (if (re-search-forward "\\(|\\{1,3\\}>?\\)" search-eol)
-        (setq insertion-char (match-string 1)))
+    (cond
+     ;; Discriminated union types
+     ((looking-at "type\\|\\s-*|.*of")
+      (setq insertion-char "|"
+            insertion-tpl type-yas-tpl))
+     ;; Match blocks
+     ((looking-at "\\s-*match .* with\\|\\s-*| .*->")
+      (setq insertion-char "|"
+            insertion-tpl match-yas-tpl))
+     ;; Arbitrary piping expressions
+     ((looking-at "\\s-*\\(|\\{1,3\\}>\\)")
+      (setq insertion-char (match-string 1))))
     (end-of-line)
     (newline-and-indent)
     (when insertion-char
       (insert insertion-char)
       (insert " ")
-      (when (string= insertion-char "|")
-        (save-excursion
-          (cond
-           ;; We're in a match block
-           ((re-search-backward "->" search-bol t) (setq insertion-tpl "$1 -> $0"))
-           ;; We're in a discriminated union
-           ((re-search-backward "of" search-bol t) (setq insertion-tpl "$1 of $0"))))
+      (when insertion-tpl
         (yas-expand-snippet insertion-tpl)))))
 
 (use-package fsharp-mode
@@ -56,11 +72,17 @@ from a usability standpoint to do so."
             (lambda ()
               (subword-mode +1)
               (setq company-auto-complete nil)))
-  (sp-local-pair 'fsharp-mode "<" ">")
-  (sp-local-pair 'fsharp-mode "@" "@"
-                 :actions '(insert wrap navigate)
-                 :when '(orary/sp-insert-at-p))
-  (sp-local-pair 'fsharp-mode "|" "|" :when '(orary/sp-insert-pipe-p))
+  (sp-with-modes 'fsharp-mode
+    (sp-local-pair "<" ">"
+                   :when '((orary/sp-close-open-angle-p))
+                   :post-handlers '(orary/sp-correct-angle-bracket))
+    (sp-local-pair "@" "@"
+                   :actions '(insert wrap navigate)
+                   :when '(orary/sp-insert-at-p))
+    (sp-local-pair "|" "|" :when '(orary/sp-insert-pipe-p))
+    (sp-local-pair "\"\"\"" "\"\"\"")
+    (sp-local-pair "`" "`" :actions :rem)
+    (sp-local-pair "``" "``"))
   (add-hook 'fsharp-mode-hook (lambda () (setq dabbrev-check-all-buffers nil)))
   :bind (:map fsharp-mode-map
               ("<C-return>" . fsharp-ret-dwim)))
