a9421a796fe78f66a97d4682f9efe4646fc407f5
Ross Donaldson <gastove@gmail.com>
2017-06-23 11:30:58 -0700

    Use gls

--- modules/orary-osx.el
+++ modules/orary-osx.el
@@ -9,10 +9,13 @@
 (use-package exec-path-from-shell)
 (exec-path-from-shell-initialize)
 (exec-path-from-shell-copy-env "WORKON_HOME")
+
 ;; OSX Modifier keys
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
+;; Use GNU `ls' instead of fucked-up custom Mac ls
+(setq insert-directory-program "gls")
 
 (provide 'orary-osx)
 ;;; orary-osx.el ends here
