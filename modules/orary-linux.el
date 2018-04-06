94fb2436ad96fbca997582398440d7f23f87fdc2
Ross Donaldson <rdonaldson@squarespace.com>
2018-04-06 09:12:07 -0700

    Whitespace

--- modules/orary-linux.el
+++ modules/orary-linux.el
@@ -7,9 +7,9 @@
 (use-package exec-path-from-shell
   :config
   (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-envs  '("SSH_AUTH_SOCK"
-                                     "SSH_AGENT_PID"
-                                     "GPG_AGENT_INFO")))
+  (exec-path-from-shell-copy-envs '("SSH_AUTH_SOCK"
+                                    "SSH_AGENT_PID"
+                                    "GPG_AGENT_INFO")))
 
 (provide 'orary-linux)
 ;;; orary-linux.el ends here
