bf048e47323692912b354aaa57c54d2aa7e37621
Ross Donaldson <rdonaldson@squarespace.com>
2018-07-27 16:24:08 -0700

    Rust configs, dwim func

--- modules/orary-rust.el
+++ modules/orary-rust.el
@@ -8,21 +8,31 @@
 (use-package racer)         ;; Symbol completion, introspection
 (use-package flycheck-rust) ;; Syntax checking
 
+(defun orary/rust-ret-dwim ()
+  (interactive)
+  (unless (eolp)
+    (re-search-forward ")"))
+  
+  (unless (or (looking-at ";")
+              (looking-back ";" (- (point) 1)))
+    (insert-char ?\;))
+  (end-of-line)
+  (newline-and-indent))
 
 ;; The Business
 (use-package rust-mode
   :config
-  (setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
+  (setq racer-cmd "~/.cargo/bin/racer") 
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
   (add-hook 'rust-mode-hook #'cargo-minor-mode)
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
-  ;; For now, this is a no-go; rustfmt is deprecated and requires a `--force'
-  ;; flag to run, but the new rustfmt-nightly is missing dylibs I can't provide.
-  ;; Cool.
-  ;; :bind (:map rust-mode-map
-  ;;             ("C-c n" . #'rust-format-buffer))
+  (rust-enable-format-on-save)
+  :bind (:map rust-mode-map
+              ("C-c n"   . #'rust-format-buffer)
+              ("C-c C-c" . #'rust-compile)
+              ("<C-return>" . #'orary/rust-ret-dwim))
   )
 
 (provide 'orary-rust)
