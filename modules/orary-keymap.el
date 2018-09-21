b290b00b3271d1ac3868c71b96a4fd766f0ae24e
Ross Donaldson <rdonaldson@squarespace.com>
2018-09-21 15:02:22 -0700

    Cleaned up keymap!
    1. Order things better. Makes it easier to read!
    2. Wire up previously unwired commands -- nice to have a bunch of them!

--- modules/orary-keymap.el
+++ modules/orary-keymap.el
@@ -8,16 +8,18 @@
                 'orary/prelude-move-beginning-of-line)
 (global-set-key (kbd "s-j") 'orary/join-below)
 (global-set-key (kbd "M-;") 'orary/comment-dwim-line)
-(global-set-key (kbd "C-c q c") 'orary/insert-signed-comment)
 (global-set-key (kbd "C-c n") 'orary/clean-and-indent-buffer)
 
-(global-set-key (kbd "C-c q t") 'orary/toggle-window-split)
+(global-set-key (kbd "C-c q c") 'orary/insert-signed-comment)
 (global-set-key (kbd "C-c q d") 'orary/insert-iso-date)
 (global-set-key (kbd "C-c q e") 'orary/insert-emote)
-(global-set-key (kbd "C-c q s") 'toggle-frame-fullscreen)
 (global-set-key (kbd "C-c q j") 'orary/pprint-json-in-new-buffer)
-(global-set-key (kbd "C-c q w") 'helm-world-time)
 (global-set-key (kbd "C-c q m") 'mu4e)
+(global-set-key (kbd "C-c q n") 'neotree-toggle)
+(global-set-key (kbd "C-c q s") 'toggle-frame-fullscreen)
+(global-set-key (kbd "C-c q t") 'orary/toggle-window-split)
+(global-set-key (kbd "C-c q w") 'helm-world-time)
+
 (global-set-key (kbd "C-;") 'flyspell-auto-correct-previous-word)
 (global-set-key (kbd "C-M-?") 'sauron-toggle-hide-show)
 
