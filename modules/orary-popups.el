712d662c7d9e55f8bd7e8bd345d8d344cdb0b9c5
Ross Donaldson <rdonaldson@squarespace.com>
2018-10-15 20:53:59 -0700

    Start working with Shackle

---
+++ modules/orary-popups.el
@@ -0,0 +1,13 @@
+;;; orary-popups.el --- Maybe I wanna use Shackle?
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(use-package shackle
+  :config
+  (setq shackle-rules '((compilation-mode :noselect t))
+        shackle-default-rule '(:select t)))
+
+(provide 'orary-popups)
+;;; orary-popups.el ends here
