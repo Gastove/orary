5dc10e86f2c6de2554947ee2bcfe608d7d03ea7a
Ross Donaldson <rdonaldson@squarespace.com>
2018-12-13 20:04:23 -0800

    Tidy whitespace

--- modules/orary-braces.el
+++ modules/orary-braces.el
@@ -1,20 +1,20 @@
 ;;; orary-braces.el --- Tweaks for managing pairs of curlys
 ;;
 ;;; Commentary:
-;; 
+;;
 ;;; Code:
 
 (defun orary/braces-open-pair ()
   "With cursor between a pair of curly braces, put point on a
 correctly indented newline with the closing brace on the next
-line."  
+line."
   (newline-and-indent)
   (beginning-of-line)
   (open-line 1)
   (indent-according-to-mode))
 
 (defun orary/braces-open-newline ()
-  (interactive)    
+  (interactive)
   (newline-and-indent)
   (forward-line -1)
   (indent-according-to-mode))
