c8bcd6378deec0223b27dd1c47a6045522ea7f9f
Ross Donaldson <gastove@gmail.com>
2018-12-21 10:30:13 -0800

    Update psc mode

--- modules/orary-purescript.el
+++ modules/orary-purescript.el
@@ -4,16 +4,20 @@
 ;;
 ;;; Code:
 
+(use-package psc-ide)
+
 ;; PureScript
 (use-package purescript-mode
   :mode "\\.purs\\'"
   :config
   (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
   (add-hook 'purescript-mode-hook
-	    (lambda ()
-	      (push '("->" . ?→) prettify-symbols-alist)
-	      (push '("=>" . ?⇒) prettify-symbols-alist)
-	      )))
+            (lambda ()
+              (push '("->" . ?→) prettify-symbols-alist)
+              (push '("=>" . ?⇒) prettify-symbols-alist)
+              (psc-ide-mode)
+              (turn-on-purescript-indentation)
+              )))
 
 (provide 'orary-purescript)
 ;;; orary-purescript.el ends here
