b8b215aba5c421ba3070802109a9298e2d1cfc73
Ross Donaldson <rdonaldson@squarespace.com>
2019-01-26 16:56:21 -0800

    Substantially improve F# configs
    1. Correct some weird problems with fsharp-ret-dwim
    2. Formatting with fantomas (still in beta)

--- modules/orary-fsharp.el
+++ modules/orary-fsharp.el
@@ -40,14 +40,14 @@ usability standpoint to do so."
   (interactive)
   (let ((search-bol (line-beginning-position))
         (search-eol (line-end-position))
-        (type-yas-tpl "$1 of $0")
+        (type-yas-tpl "$1${2: of $0}")
         (match-yas-tpl "$1 -> $0")
         insertion-char
         insertion-tpl)
     (beginning-of-line)
     (cond
      ;; Discriminated union types
-     ((looking-at "type\\|\\s-*|.*of")
+     ((looking-at "\\s-*type\\|\\s-*| [A-Za-z'_]+\\(?: of.*\\)?$")
       (setq insertion-char "|"
             insertion-tpl type-yas-tpl))
      ;; Match blocks
@@ -58,12 +58,41 @@ usability standpoint to do so."
      ((looking-at "\\s-*\\(|\\{1,3\\}>\\)")
       (setq insertion-char (match-string 1))))
     (end-of-line)
-    (newline-and-indent)
+    (fsharp-newline-and-indent)
     (when insertion-char
       (insert insertion-char)
       (insert " ")
       (when insertion-tpl
         (yas-expand-snippet insertion-tpl)))))
+
+(defun orary/make-fantomas-tmp-buffer ()
+  (-let [buf (get-buffer-create "*Fantomas Output*")]
+    (with-current-buffer buf
+      (fsharp-mode)
+      (read-only-mode))
+    buf))
+
+(defun orary/fsharp-format-buffer (arg)
+  "Formats the current buffer with `fantomas' if installed; see
+  https://github.com/fsprojects/fantomas for details. With prefix
+  argument ARG, shows a preview of the output instead of
+  overwriting the current buffer."
+  (interactive "P")
+  (-if-let (fantomas-command (executable-find "fantomas"))
+      (-let [target-buffer (if arg
+                               (orary/make-fantomas-tmp-buffer)
+                             (buffer-name))]
+        (shell-command-on-region
+         (point-min)
+         (point-max)
+         (s-join " "
+                 (list
+                  "fantomas --stdin --stdout"
+                  "--pageWidth" (number-to-string fill-column)
+                  "--indent" (number-to-string tab-width)))
+         target-buffer))
+    (message "Couldn't find `fantomas' on your path"))
+  )
 
 (use-package fsharp-mode
   :mode "\\.fs[iylx]?$"
