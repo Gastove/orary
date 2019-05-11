0b622d7703753f1ee5bfb9868cb3992c0e202f7a
Ross Donaldson <gastove@gmail.com>
2019-05-11 11:27:20 -0700

    Treemacs!
    Oh man treemacs is *neat* -- like Neotree, but maintained, and lets you drill
    down into actual declarations in the file

---
+++ modules/orary-tree.el
@@ -0,0 +1,37 @@
+;;; orary-tree.el --- Tree-based browsing, editing, file management, etc
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+;; Neotree is unmaintained and buggy. We'll try Treemacs for a bit, see if its as nice.
+;; Neotree
+;; (use-package neotree
+;;   :config
+;;   (setq neo-theme (if (display-graphic-p) 'icons 'arrow)
+;;         neo-window-fixed-size t
+;;         neo-window-width 100
+;;         neo-window-position 'left))
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window)))
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(provide 'orary-tree)
+;;; orary-tree.el ends here
