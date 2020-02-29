56f5819b84a3306da8d846b79b1da322c8e4f05e
Ross Donaldson <rdonaldson@squarespace.com>
2020-02-29 09:30:11 -0800

    Add a module for C configs
    Ugh.

---
+++ modules/orary-c.el
@@ -0,0 +1,11 @@
+;;; orary-c.el --- C/++ Configs for Orary
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(add-hook 'c++-mode-hook
+          (lambda () (lsp)))
+
+(provide 'orary-c)
+;;; orary-c.el ends here
