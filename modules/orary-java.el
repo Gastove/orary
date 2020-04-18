2f01155cd848b07e5f90bae150bfa8776e6cc949
Ross Donaldson <rdonaldson@squarespace.com>
2020-04-18 12:33:37 -0700

    fix java lsp

--- modules/orary-java.el
+++ modules/orary-java.el
@@ -4,22 +4,6 @@
 ;;
 ;;; Code:
 
-
-;; (when (and lsp-eldoc-enable-signature-help (lsp--capability "signatureHelpProvider"))
-;;         (cl-incf pending)
-;;         (lsp-request-async
-;;          "textDocument/signatureHelp"
-;;          (lsp--text-document-position-params)
-;;          (lambda (signature)
-;;            (when (eq request-id lsp-hover-request-id)
-;;              (when-let (message (and signature (lsp--signature->eldoc-message signature)))
-;;                (when (or (and lsp-eldoc-prefer-signature-help (setq pending 1))
-;;                          (not lsp--eldoc-saved-message))
-;;                  (setq lsp--eldoc-saved-message message)))
-;;              (when (zerop (cl-decf pending))
-;;                (lsp--eldoc-message lsp--eldoc-saved-message))))
-;;          :error-handler #'ignore))
-
 (defun orary/write-javadoc (signature)
   (message "Got signature:\n%s" signature)
   (message "Has keys:\n %s" (gethash "signatures" signature)))
@@ -35,13 +19,9 @@
   :ensure t
   :after lsp
   :config
-  (setq lsp-log-io 't)
+  (setq lsp-log-io 't
+        lsp-java-vmargs '("-noverify" "-Xmx4G" "-XX:+UseG1GC" "-XX:+UseStringDeduplication"))
   (require 'dap-java)
-  (add-hook 'java-mode-hook
-            (lambda ()
-              (lsp)
-              (subword-mode +1)
-              ))
   :bind (:map lsp-language-map
               ("r i" . #'lsp-java-add-import)
               ("r t" . #'lsp-java-add-throws)
@@ -51,6 +31,11 @@
               ("g g" . #'lsp-java-generate-getters-and-setters)
               ("g t" . #'lsp-java-generate-to-string)))
 
+(add-hook 'java-mode-hook
+          (lambda ()
+            (lsp)
+            (subword-mode +1)
+            ))
 
 (provide 'orary-java)
 ;;; orary-java.el ends here
