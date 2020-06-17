7f06f0f9c5b80b89d8756099f95760879f04c3c1
Ross Donaldson <gastove@gmail.com>
2020-06-17 08:35:08 -0700

    lets start the switch to lsp for python

--- modules/orary-python.el
+++ modules/orary-python.el
@@ -12,22 +12,18 @@
 
 (setq python-shell-interpreter "python3")
 
-(use-package elpy
-  :demand t
+(use-package lsp-python-ms
+  :ensure t
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-python-ms)
+                         (lsp)))
   :config
-
-  (elpy-enable)
-  (require 'electric)
-
   (setq
    python-shell-interpreter "ipython"
    python-shell-interpreter-args "--simple-prompt -i"
    python-fill-docstring-style 'django
    python-environment-directory "~/.python_virtualenvs/"
-   ;; There is some indication this might help with the vague weirds I've seen.
-   ;; https://github.com/jorgenschaefer/elpy/issues/985#issuecomment-249113499
-   ;; python-shell-completion-native-enable nil
-   elpy-rpc-python-command "python3"
 
    ;; My proud contribution
    python-indent-def-block-scale 1
@@ -58,12 +54,60 @@
 
 
   (add-hook 'python-mode-hook #'orary/python-mode-settings)
-  :bind (:map python-mode-map
-              ("C-c q i" . orary/python-make-module)
-              ("C-c q q" . orary/replace-double-quote-with-single)
-              ("C-c q r" . recompile)
-              ("C-c n"   . elpy-black-fix-code )
-              ("<M-RET>" . orary/rotate)))
+  )
+
+;; (use-package elpy
+;;   :demand t
+;;   :config
+
+;;   (elpy-enable)
+;;   (require 'electric)
+
+;;   (setq
+;;    python-shell-interpreter "ipython"
+;;    python-shell-interpreter-args "--simple-prompt -i"
+;;    python-fill-docstring-style 'django
+;;    python-environment-directory "~/.python_virtualenvs/"
+;;    ;; There is some indication this might help with the vague weirds I've seen.
+;;    ;; https://github.com/jorgenschaefer/elpy/issues/985#issuecomment-249113499
+;;    ;; python-shell-completion-native-enable nil
+;;    elpy-rpc-python-command "python3"
+
+;;    ;; My proud contribution
+;;    python-indent-def-block-scale 1
+;;    )
+
+;;   (defun orary/python-mode-settings ()
+;;     (subword-mode +1)
+;;     (setq-local electric-layout-rules
+;;                 '((?: . (lambda ()
+;;                           (and (zerop (first (syntax-ppss)))
+;;                                (python-info-statement-starts-block-p)
+;;                                'after)))))
+;;     (add-hook 'post-self-insert-hook
+;;               #'electric-layout-post-self-insert-function nil 'local)
+;;     (when (fboundp #'python-imenu-create-flat-index)
+;;       (setq-local imenu-create-index-function
+;;                   #'python-imenu-create-flat-index))
+;;     (setq fill-column 79))
+
+;;   (defun orary/python-make-module ()
+;;     (interactive)
+;;     (let ((target (expand-file-name "__init__.py")))
+;;       (if (file-exists-p target)
+;;           (message "Init file already exists!")
+;;         (progn
+;;           (write-region "" nil target)
+;;           (message "Init file created for module")))))
+
+
+;;   (add-hook 'python-mode-hook #'orary/python-mode-settings)
+;;   :bind (:map python-mode-map
+;;               ("C-c q i" . orary/python-make-module)
+;;               ("C-c q q" . orary/replace-double-quote-with-single)
+;;               ("C-c q r" . recompile)
+;;               ("C-c n"   . elpy-black-fix-code )
+;;               ("<M-RET>" . orary/rotate)))
 
 ;; Jupyter Notebooks
 (require 'company)
