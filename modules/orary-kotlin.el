bfcfdb02245aa3d6adab1d4ca13d1a86b96091c2
Ross Donaldson <rdonaldson@squarespace.com>
2021-05-18 08:27:15 -0700

    Snippets, a little Kotlin setup

---
+++ modules/orary-kotlin.el
@@ -0,0 +1,10 @@
+;;; orary-kotlin.el --- Kotlin configs for Orary
+;;
+;;; Commentary:
+;; Sometimes, your company decides to switch to Kotlin over Groovy for Gradle.
+;;; Code:
+
+(use-package kotlin-mode)
+
+(provide 'orary-kotlin)
+;;; orary-kotlin.el ends here
