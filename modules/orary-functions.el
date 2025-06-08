398a3004034ba89bb3b69de57f9ac74955528ff4
Ross Donaldson <rdonaldson@squarespace.com>
2025-06-08 12:13:49 -0700

    WIP: python code blocks in Nix integration tests

--- modules/orary-functions.el
+++ modules/orary-functions.el
@@ -471,5 +471,50 @@ buffer. E.g., if the buffer is visiting
        (file-name-nondirectory)
        (file-name-sans-extension)))
 
+;;------------- edit-indirect prototype for Nix integration tests -------------;;
+;; We're searching between:
+;; Beginning: testScript = ''
+;; End:       '';
+(defun lsp-nix--get-script-bounds ()
+  (save-excursion
+   (-let ((begin (search-backward "testScript = ''"))
+          (end (search-forward "'';")))
+     (list begin end))))
+
+(defun lsp-nix-edit-python-code-block ()
+  "Edit a NixOS integration test python script in an indirect buffer."
+  (interactive)
+  (save-excursion
+    (if (fboundp 'edit-indirect-region)
+        (let* ((bounds (markdown-get-enclosing-fenced-block-construct))
+               (begin (and bounds (not (null (nth 0 bounds))) (goto-char (nth 0 bounds)) (line-beginning-position 2)))
+               (end (and bounds(not (null (nth 1 bounds)))  (goto-char (nth 1 bounds)) (line-beginning-position 1))))
+          (if (and begin end)
+              (let* ((indentation (and (goto-char (nth 0 bounds)) (current-indentation)))
+                     (lang (markdown-code-block-lang))
+                     (mode (or (and lang (markdown-get-lang-mode lang))
+                               markdown-edit-code-block-default-mode))
+                     (edit-indirect-guess-mode-function
+                      (lambda (_parent-buffer _beg _end)
+                        (funcall mode)))
+                     (indirect-buf (edit-indirect-region begin end 'display-buffer)))
+                ;; reset `sh-shell' when indirect buffer
+                (when (and (not (member system-type '(ms-dos windows-nt)))
+                           (member mode '(shell-script-mode sh-mode))
+                           (member lang (append
+                                         (mapcar (lambda (e) (symbol-name (car e)))
+                                                 sh-ancestor-alist)
+                                         '("csh" "rc" "sh"))))
+                  (with-current-buffer indirect-buf
+                    (sh-set-shell lang)))
+                (when (> indentation 0) ;; un-indent in edit-indirect buffer
+                  (with-current-buffer indirect-buf
+                    (indent-rigidly (point-min) (point-max) (- indentation)))))
+            (user-error "Not inside a GFM or tilde fenced code block")))
+      (when (y-or-n-p "Package edit-indirect needed to edit code blocks. Install it now? ")
+        (progn (package-refresh-contents)
+               (package-install 'edit-indirect)
+               (markdown-edit-code-block))))))
+
 (provide 'orary-functions)
 ;;; orary-functions.el ends here
